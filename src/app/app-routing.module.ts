e62d6ca866a952c16d13ae7cd5126b72e3a81a99
mikejunt <mikejunt@gmail.com>
2020-03-05 12:15:29 -0600

    last basic config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
+import { NewReviewComponent } from './new-review/new-review.component';
+import { ParkSummaryComponent } from './park-summary/park-summary.component';
+import { LoginComponent } from './login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent },
+  {path: 'signup',  component: SignupComponent},
+  {path: 'newreview', component: NewReviewComponent},
+  {path: 'parkreview', component: ParkSummaryComponent},
+  {path: '', redirectTo: 'login', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
