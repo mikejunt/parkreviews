aeb1a182fd8ba444251d49fcc4296707f043d732
Shannon Sokolik <shannonsokolik@Shannons-MacBook-Air.local>
2020-03-05 16:51:18 -0600

    confirm password matcher works

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../services/user.service';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { CrossFieldMatcher } from '../shared/crossfield.matcher';
+import { passwordMatchValidator } from '../shared/password-match.validator'
 
 @Component({
   selector: 'app-signup',
@@ -8,22 +10,23 @@ import { FormGroup, Validators, FormBuilder } from '@angular/forms';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
-  signupForm: FormGroup
+  signupForm = this.formBuilder.group({
+    username: ['', Validators.compose([Validators.required, Validators.maxLength(12), Validators.minLength(4)])],
+    password: ['', Validators.compose([Validators.required, Validators.maxLength(32), Validators.minLength(6)])],
+    confirm: [''],
+  }, { validator: passwordMatchValidator })
+  matcher: CrossFieldMatcher
   hide: boolean = true;
   constructor(private userService: UserService, private formBuilder: FormBuilder) { }
 
   signup(e){
     e.preventDefault();
+    console.log(this.signupForm.valid)
     if(this.signupForm.valid){
       this.userService.signup(this.signupForm.value.username, this.signupForm.value.password);
     }
   }
-  ngOnInit(): void {
-    this.signupForm = this.formBuilder.group({
-      username: ['', Validators.compose([Validators.required, Validators.maxLength(16), Validators.minLength(3)])],
-      password: ['', Validators.compose([Validators.required, Validators.maxLength (128), Validators.minLength(8)])]
-    });
-  }
+  ngOnInit(): void { }
   
 }
 
