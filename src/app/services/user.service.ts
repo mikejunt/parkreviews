ce6dccbdc10695c74bc12331ea4a1c29257cc366
mikejunt <mikejunt@gmail.com>
2020-03-06 13:16:52 -0600

    selective queries work

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -5,7 +5,7 @@ import { Store } from '@ngrx/store';
 import { AppState } from '../store';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
-import { map, tap } from 'rxjs/operators'
+import { map, tap, first } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
@@ -13,38 +13,51 @@ import { map, tap } from 'rxjs/operators'
 export class UserService {
   userDB: AngularFirestoreCollection<any> = this.db.collection('users')
   constructor(private router: Router, private store: Store<AppState>, private db: AngularFirestore) {
-    
+
   }
 
   userQuery(key: string, operator: firebase.firestore.WhereFilterOp, value: string) {
-    let dbquery:AngularFirestoreCollection<any> = this.db.collection('users', (ref)=> ref.where(key, operator, value))
+    let dbquery: AngularFirestoreCollection<any> = this.db.collection('users', (ref) => ref.where(key, operator, value))
     let dbquery$ = dbquery.snapshotChanges().pipe(map(actions => {
       return actions.map(x => {
         const data = x.payload.doc.data();
         const id = x.payload.doc.id;
-        return { id, ...data}  
+        return { id, ...data }
       })
     }))
-    let queryresult
-    dbquery$.subscribe(res => {queryresult = res;console.log(res, "from observable")})
-    return queryresult
+    // let queryresult
+    // dbquery$.subscribe(res => {queryresult = res;console.log(res, "from observable")})
+    // return queryresult
+    return dbquery$
   }
 
-  login(username, password){
-    this.userQuery("username", "==", username)
-    this.store.dispatch(Actions.setActiveUser({username}))
-    console.log(username,password)
-      }
+  login(username, password) {
+    this.userQuery("username", "==", username).pipe(first()).subscribe(res => {
+      if (res.length != 1) { console.log("No user with that name"); return false }
+      // pwd hash?
+      if (res["password"] != password) { console.log("Password doesn't match"); return false }
+      this.store.dispatch(Actions.setActiveUser({ username }))
+      this.router.navigate(['/newreview'])
+    })
+  }
 
-  logout(){
+  logout() {
     this.store.dispatch(Actions.clearActiveUser())
   }
 
-  signup(username, password){
-    const id = this.db.createId()
-    const user = {id: id, username: username, password: password}
-    this.userDB.doc(id).set(user)
-    console.log(user, "From signup function")
+  signup(username, password) {
+    this.userQuery("username", "==", username).pipe(first()).subscribe(res => {
+      console.log(res)
+      if (res.length != 0) { console.log("Duplicate Username"); return false }
+      // hash password here
+      else {
+        const id = this.db.createId()
+        const user = { id: id, username: username, password: password }
+        this.userDB.doc(id).set(user)
+        console.log(user, "From signup function")
+        return true
+      }
+    })
   }
-  
+
 }
