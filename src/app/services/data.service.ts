30557d66f4eeb31db17adee2463398d779a4b758
mikejunt <mikejunt@gmail.com>
2020-03-07 14:15:16 -0600

    refactors queries to single source

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestoreCollection, AngularFirestore } from '@angular/fire/firestore';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private db: AngularFirestore) { }
+
+  dataQuery(collection: string, key: string, operator: firebase.firestore.WhereFilterOp, value: string): Observable<any[]> {
+    const dbquery: AngularFirestoreCollection<any> = this.db.collection(collection, (ref) => ref.where(key, operator, value))
+    const dbquery$ = dbquery.snapshotChanges().pipe(map(actions => {
+      return actions.map(x => {
+        const data = x.payload.doc.data();
+        const id = x.payload.doc.id;
+        return { id, ...data }
+      })
+    }))
+    return dbquery$
+  }
+
+  dataSave(collection: string, document: Object, options?: firebase.firestore.SetOptions) {
+    const database: AngularFirestoreCollection<any> = this.db.collection(collection)
+    const id = this.db.createId()
+    const doc = { ...document, id: id }
+    if (options) { database.doc(id).set(doc, options) }
+    else { database.doc(id).set(doc) }
+  }
+
+  getWholeCollection(collection: string): Observable<any[]> {
+    let collect: Array<any>
+    let database = this.db.collection(collection)
+    let database$ = database.valueChanges()
+    return database$
+  }
+
+}
