ecefdaa54d8c56706ddec258a4e930eb9e47ff0e
mikejunt <mikejunt@gmail.com>
2020-03-08 12:50:48 -0500

    changes routing for login

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -24,17 +24,9 @@ export class UserService {
         console.log(res, "from userdb")
         let user = res[0]
         this.store.dispatch(Actions.setActiveUser({activeuser: user}))
-        this.router.navigate(['parkreview/1'])
+        this.router.navigate(['newreview'])
       })
     })
-    // this.data.dataQuery("users", "username", "==", username).pipe(first()).subscribe(res => {
-    //   if (res.length != 1) { console.log("No user with that name"); return false }
-    //   if (res["password"] != password) { console.log("Password doesn't match"); return false }
-    //   let activeuser = {id: res["id"], username: res["username"], zipcode: "68134"}
-    //   this.store.dispatch(Actions.setActiveUser({ activeuser }))
-    //   this.router.navigate(['/newreview'])
-    // })
-    
   }
 
 
@@ -53,7 +45,7 @@ export class UserService {
       let stateuser = {id: response.user.uid, username: username, email: email, zipcode: zipcode}
       console.log(stateuser, "userdata sent to state")
       this.store.dispatch(Actions.setActiveUser({activeuser: stateuser}))
-      this.router.navigate(['parkreview/1'])
+      this.router.navigate(['newreview'])
     }).catch(err=>console.log("error:",err))
   }
 
