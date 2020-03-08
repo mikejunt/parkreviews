5c0d9fdc488c87d7414d0a3fb24d47ffdb95842b
mikejunt <mikejunt@gmail.com>
2020-03-08 12:40:17 -0500

    fire auth integrated to login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -14,12 +14,12 @@ export class LoginComponent implements OnInit {
   login(e){
     e.preventDefault();
     if(this.loginForm.valid){
-      this.userService.login(this.loginForm.value.username, this.loginForm.value.password);
+      this.userService.login(this.loginForm.value.email, this.loginForm.value.password);
     }
   }
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
-      username: ['', Validators.compose([Validators.required, Validators.maxLength(16), Validators.minLength(3)])],
+      email: ['', Validators.compose([Validators.required, Validators.maxLength(32), Validators.minLength(3)])],
       password: ['', Validators.compose([Validators.required, Validators.maxLength (128), Validators.minLength(8)])]
     });
   }
